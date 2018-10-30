061965c21d6dc523b23a4818e1a87c64053a41d6
Spencer Croy <spencerccroy@gmail.com>
2018-10-30 17:28:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,5 +7,5 @@ def display_rainbow(array_colors)
         #{letters[3]:array_colors[3]},
         #{letters[4]:array_colors[4]},
         #{letters[5]:array_colors[5]},
-        #{letters[6]:array_colors[6]},"
+        #{letters[6]:array_colors[6]}"
 end
